d2a5f6f31521de2f300f35b3579cad4f639b9c04
freeman6-dev <pawel.jab21@gmail.com>
2021-02-03 20:02:08 +0100

    configuration_and_first_views

---
+++ charity_donation_app/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import View
+
+
+class IndexView(View):
+
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+class LoginView(View):
+
+    def get(self, request):
+        return render(request, 'login.html')
+
+
+class RegisterView(View):
+
+    def get(self, request):
+        return render(request, 'register.html')
+
+
+class AddDonationView(View):
+
+    def get(self, request):
+        return render(request, 'form.html')
